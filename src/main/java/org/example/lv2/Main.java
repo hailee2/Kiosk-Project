9fb9a05af70f1797251b105eab8da35914a5001e
hailee2 <slowslow9150@gmail.com>
2025-07-16 16:38:32 +0900

    Lv2/MenuItem 클래스 생성

--- src/main/java/org/example/lv2/Main.java
+++ src/main/java/org/example/lv2/Main.java
@@ -3,5 +3,8 @@ package org.example.lv2;
 public class Main {
     public static void main(String[] args) {
 
+        MenuItem menu1 = new MenuItem("햄버거", 7000,"진짜 맛있음");
+
+        menu1.printMenu();
     }
 }
--- src/main/java/org/example/lv2/MenuItem.java
+++ src/main/java/org/example/lv2/MenuItem.java
@@ -1,4 +1,22 @@
 package org.example.lv2;
 
 public class MenuItem {
+    //속성
+    String name;
+    int price;
+    String description;
+
+
+    //생성자
+    public MenuItem (String name, int price, String description){
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+
+    //기능
+    public void printMenu(){
+        System.out.println("선택한 메뉴: " + name + ", " + price + "원" + ", " + description);
+    }
 }

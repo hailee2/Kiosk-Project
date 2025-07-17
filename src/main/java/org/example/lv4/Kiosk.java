84643027d12b683dbfb3d1be7b3654be7b9e0b80
hailee2 <slowslow9150@gmail.com>
2025-07-17 19:37:16 +0900

    add List/create Menu Class

--- src/main/java/org/example/lv4/Kiosk.java
+++ src/main/java/org/example/lv4/Kiosk.java
@@ -1,4 +1,5 @@
 package org.example.lv4;
 
 public class Kiosk {
+
 }
--- src/main/java/org/example/lv4/MenuItem.java
+++ src/main/java/org/example/lv4/MenuItem.java
@@ -1,4 +1,37 @@
 package org.example.lv4;
 
 public class MenuItem {
+    //속성
+    String name;
+    double price;
+    String description;
+
+
+    //생성자
+    public MenuItem(String name){
+        this.name= name;
+    }
+
+    public MenuItem(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public MenuItem(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    //기능
+    //toString() 오버라이딩
+    public String toString() {
+        if(!(name==null) && price==0.0 && description==null){
+            return name;
+        }else if(!(name==null) && !(price==0.0) && description==null){
+            return name +" | "+ price +" | ";
+        }else{
+            return name +" | "+ price +" | "+ description;
+        }
+    }
 }

f67e84b18379afe89121fc027289ddad2145666a
hailee2 <slowslow9150@gmail.com>
2025-07-18 18:52:25 +0900

    add start method

--- src/main/java/org/example/lv4/Kiosk.java
+++ src/main/java/org/example/lv4/Kiosk.java
@@ -6,10 +6,16 @@ import java.util.Scanner;
 public class Kiosk {
     //속성
     List<Menu> menus;
+    List<MenuItem> items1;
+    List<MenuItem> items2;
+    List<MenuItem> items3;
 
     //생성자
-    public Kiosk(List<Menu> menus){
+    public Kiosk(List<Menu> menus, List<MenuItem> items1, List<MenuItem> items2, List<MenuItem> items3){
         this.menus=menus;
+        this.items1=items1;
+        this.items2=items2;
+        this.items3=items3;
     }
 
     //기능
@@ -18,6 +24,81 @@ public class Kiosk {
         Scanner sc = new Scanner(System.in);
         // 반복문 시작
         while(true){
+            // List와 Menu 클래스 활용하여 상위 카테고리 메뉴 출력
+            System.out.println(menus);
+            // 숫자 입력 받기
+            System.out.print("입력: ");
+            String selectMain = sc.nextLine();
+                switch (selectMain){
+                    case "1":
+                        for (MenuItem item : items1) {
+                            System.out.println(item);
+                        }
+                            System.out.print("입력: ");
+                            String selectBurger = sc.nextLine();
+                                switch(selectBurger){
+                                    case "1":
+                                        System.out.println(items1.get(0));
+                                        break;
+                                    case "2":
+                                        System.out.println(items1.get(1));
+                                        break;
+                                    case "3":
+                                        System.out.println(items3.get(2));
+                                        break;
+                                    case "4":
+                                        System.out.println(items3.get(3));
+                                        break;
+                                    case "0":
+                                        break;
+                                }// 근데 여기서어떻게 딱! 햄버거아이템리스트를 가져올 수가 있나요...
+                        break;
+                    case "2":
+                        for (MenuItem item : items2) {
+                            System.out.println(item);
+                        }
+                        System.out.print("입력: ");
+                        String selectDrink = sc.nextLine();
+                        switch(selectDrink){
+                            case "1":
+                                System.out.println(items2.get(0));
+                                break;
+                            case "2":
+                                System.out.println(items2.get(1));
+                                break;
+                            case "3":
+                                System.out.println(items2.get(2));
+                                break;
+                            case "0":
+                                break;
+                        }
+                        break;
+                    case "3":
+                        for (MenuItem item : items3) {
+                            System.out.println(item);
+                        }
+                        System.out.print("입력: ");
+                        String selectDesert = sc.nextLine();
+                        switch(selectDesert){
+                            case "1":
+                                System.out.println(items3.get(0));
+                                break;
+                            case "2":
+                                System.out.println(items3.get(1));
+                                break;
+                            case "3":
+                                System.out.println(items3.get(2));
+                                break;
+                            case "0":
+                                break;
+                        }
+                        break;
+                    case "0":
+                    return;
+                }
+
+            // 입력 받은 숫자가 올바르다면 인덱스로 활용하여 List에 접근하기
+            // List<Menu>에 인덱스로 접근하면 Menu만 추출할 수 있겠죠?
 
         }
     }
--- src/main/java/org/example/lv4/Main.java
+++ src/main/java/org/example/lv4/Main.java
@@ -13,28 +13,28 @@ public class Main {
         burgerList.add(new MenuItem(0,"뒤로가기"));
         List<MenuItem> drinkList = new ArrayList<>();
         drinkList.add(new MenuItem(1,"콜라",2.3));
-        drinkList.add(new MenuItem(1,"사이다",2.2));
-        drinkList.add(new MenuItem(1,"환타",2.0));
+        drinkList.add(new MenuItem(2,"사이다",2.2));
+        drinkList.add(new MenuItem(3,"환타",2.0));
         drinkList.add(new MenuItem(0,"뒤로가기"));
         List<MenuItem> desertList = new ArrayList<>();
         desertList.add(new MenuItem(1,"소프트콘",0.9));
-        desertList.add(new MenuItem(1,"감자튀김",2.5));
-        desertList.add(new MenuItem(1,"너겟",3.3));
-        desertList.add(new MenuItem(1,"뒤로가기"));
+        desertList.add(new MenuItem(2,"감자튀김",2.5));
+        desertList.add(new MenuItem(3,"너겟",3.3));
+        desertList.add(new MenuItem(0,"뒤로가기"));
 
         // Menu 객체 생성을 통해 이름 설정
         List<Menu> mainList = new ArrayList<>();
-        mainList.add(new Menu(1,"Burgers",burgerList));
-        mainList.add(new Menu(2,"Drinks",drinkList));
-        mainList.add(new Menu(3,"Deserts",desertList));
+        mainList.add(new Menu(1,"Burgers"));
+        mainList.add(new Menu(2,"Drinks"));
+        mainList.add(new Menu(3,"Deserts"));
         mainList.add(new Menu(0,"종료"));
 
-        Menu menu = new Menu();
+        Menu menu = new Menu(); // 얘가 필요해?
         //확인용 출력
         menu.printList(mainList);
 
         // Kiosk 객체 생성
-        Kiosk kiosk = new Kiosk(mainList);
+        Kiosk kiosk = new Kiosk(mainList, burgerList, drinkList, desertList);
 
         // Kiosk 내 시작하는 함수 호출
         kiosk.start();

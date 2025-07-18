8d0c93ddd5c95d31f882f86b53bc3c1334bebddb
hailee2 <slowslow9150@gmail.com>
2025-07-18 10:08:58 +0900

    add getter in Menu class

--- src/main/java/org/example/lv4/Menu.java
+++ src/main/java/org/example/lv4/Menu.java
@@ -7,15 +7,18 @@ public class Menu {
 
     // MenuItem 클래스를 List로 관리
     //속성
-    List<MenuItem> mainMenu;
-    List<MenuItem> burgers;
-    List<MenuItem> drinks;
-    List<MenuItem> deserts;
+    private List<MenuItem> menuItems;
+    List<MenuItem> mainList;
+    List<MenuItem> burgerList;
+    List<MenuItem> drinkList;
+    List<MenuItem> desertList;
 
     //생성자
     public Menu(List<MenuItem> menu){
         new ArrayList<>();
     }
+
+    public Menu() {}
 
 
     // List에 들어있는 MenuItem을 순차적으로 보여주는 함수
@@ -26,6 +29,9 @@ public class Menu {
     }
 
     // List를 리턴하는 함수
+    public List<MenuItem> getMenuItems(){
+        return menuItems;
+    }
 
     // 구조에 맞게 함수를 선언해놓고 가져다 사용하세요.
 }
